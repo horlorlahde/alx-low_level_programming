9f1da24e5730fcd77d011b2ed9f00f8486b04917
horlorlahde <mr.jahmalexamportal@gmail.com>
2022-12-09 03:05:20 -0800

    klnsfkjnsrjkf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include  <stdio.h>
+/*
+ * main - prints out result using printf
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\"");
+	return (0);
+}
